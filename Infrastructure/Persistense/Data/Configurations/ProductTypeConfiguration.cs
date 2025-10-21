04a5d04fb8273f22b1c2cbcb1fe537e7baec4f8e
عاصم محمد محمد يوسف مسعود <125200754@sha.edu.eg>
2025-10-21 12:22:24 +0300

    Product Module - DbContext, Configurations,  Migrations

---
+++ Infrastructure/Persistense/Data/Configurations/ProductTypeConfiguration.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Store.G04.Domain.Entities.Products;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Store.G04.Persistence.Data.Configurations
+{
+    public class ProductTypeConfiguration : IEntityTypeConfiguration<ProductType>
+    {
+        public void Configure(EntityTypeBuilder<ProductType> builder)
+        {
+            builder.Property(pt => pt.Name)
+                .HasColumnType("varchar")
+                .HasMaxLength(256);
+        }
+    }
+}
---
+++ Infrastructure/Persistense/Data/Contexts/StoreDbContext.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Store.G04.Domain.Entities.Products;
+using Store.G04.Persistence.Data.Configurations;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Store.G04.Persistence.Data.Contexts
+{
+    // CLR
+    public class StoreDbContext : DbContext
+    {
+
+        public StoreDbContext(DbContextOptions<StoreDbContext> options) : base(options)
+        {
+
+        }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<ProductBrand> ProductBrands { get; set; }
+        public DbSet<ProductType> ProductTypes { get; set; }
+
+        override protected void OnModelCreating(ModelBuilder modelBuilder)
+        {
+
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+
+            /*
+            modelBuilder.ApplyConfiguration(new ProductConfiguration());
+            modelBuilder.ApplyConfiguration(new ProductBrandConfiguration());
+            modelBuilder.ApplyConfiguration(new ProductTypeConfiguration());
+            */
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
