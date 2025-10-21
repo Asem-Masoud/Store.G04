f1c4af45e3ff5800ffc5f1a26a671c2e8fd8e85f
عاصم محمد محمد يوسف مسعود <125200754@sha.edu.eg>
2025-10-21 22:53:11 +0300

    Unit Of Work

---
+++ Core/Store.G04.Domain/Contracts/IUnitOfWork.cs
@@ -0,0 +1,20 @@
+﻿using Store.G04.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Store.G04.Domain.Contracts
+{
+    public interface IUnitOfWork
+    {
+        // Generic Repositories
+
+        IGenaricRepository<TKey, TEntity> GetRepository<TKey, TEntity>() where TEntity : BaseEntity<TKey>;
+
+        // Save Changes
+        Task<int> SaveChangesAsync();
+
+    }
+}
---
+++ Infrastructure/Persistense/UnitOfWork.cs
@@ -0,0 +1,43 @@
+﻿using Store.G04.Domain.Contracts;
+using Store.G04.Domain.Entities;
+using Store.G04.Persistence.Data.Contexts;
+using Store.G04.Persistence.Repositories;
+using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Store.G04.Persistence
+{
+    public class UnitOfWork(StoreDbContext _context) : IUnitOfWork
+    {
+        private ConcurrentDictionary<string, object> _repositories = new ConcurrentDictionary<string, object>();
+
+        public IGenaricRepository<TKey, TEntity> GetRepository<TKey, TEntity>() where TEntity : BaseEntity<TKey>
+        {
+            return (IGenaricRepository<TKey, TEntity>)_repositories.GetOrAdd(typeof(TEntity).Name, new GenericRepository<TKey, TEntity>(_context));
+        }
+
+
+        /*
+        private Dictionary<string, object> _repositories = new Dictionary<string, object>();
+        public IGenaricRepository<TKey, TEntity> GetRepository<TKey, TEntity>() where TEntity : BaseEntity<TKey>
+        {
+            var type = typeof(TEntity).Name;
+            if (!_repositories.ContainsKey(type))
+            {
+                var repository = new GenericRepository<TKey, TEntity>(_context);
+                _repositories.Add(type, repository);
+            }
+            return (IGenaricRepository<TKey, TEntity>)_repositories[type];
+        }
+        */
+
+        public Task<int> SaveChangesAsync()
+        {
+            return _context.SaveChangesAsync();
+        }
+    }
+}
