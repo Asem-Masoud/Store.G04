4bd02aa7cc44dbc348259330a76cba4e76969083
عاصم محمد محمد يوسف مسعود <125200754@sha.edu.eg>
2025-10-21 22:24:24 +0300

    Generic Repository

---
+++ Infrastructure/Persistense/Repositories/GenericRepository.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Store.G04.Domain.Contracts;
+using Store.G04.Domain.Entities;
+using Store.G04.Persistence.Data.Contexts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Store.G04.Persistence.Repositories
+{
+    public class GenericRepository<TKey, TEntity>(StoreDbContext _context) : IGenaricRepository<TKey, TEntity> where TEntity : BaseEntity<TKey>
+    {
+
+
+        public async Task<IEnumerable<TEntity>> GetAllAsync(bool changeTracker = false)
+        {
+            return changeTracker ?
+                await _context.Set<TEntity>().ToListAsync()
+                : await _context.Set<TEntity>().AsNoTracking().ToListAsync();
+        }
+
+
+        public async Task<TEntity?> GetAsync(TKey key)
+        {
+            return await _context.Set<TEntity>().FindAsync(key);
+        }
+
+        public async Task AddAsync(TEntity entity)
+        {
+            await _context.Set<TEntity>().AddAsync(entity);
+        }
+
+        public void Update(TEntity entity)
+        {
+            _context.Set<TEntity>().Update(entity);
+        }
+
+        public void Delete(TEntity entity)
+        {
+            _context.Set<TEntity>().Remove(entity);
+        }
+    }
+}
