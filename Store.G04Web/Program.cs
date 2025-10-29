490411e511d6b320d5b146bdb93de459d9c92666
عاصم محمد محمد يوسف مسعود <125200754@sha.edu.eg>
2025-10-29 22:43:00 +0300

    Refactoring Main Function

--- Store.G04Web/Program.cs
+++ Store.G04Web/Program.cs
@@ -8,6 +8,7 @@ using Store.G04.Services;
 using Store.G04.Services.Abstractions;
 using Store.G04.Services.Mapping.Products;
 using Store.G04.Shared.ErrorModels;
+using Store.G04Web.Extensions;
 using Store.G04Web.Middlewares;
 using System.ComponentModel;
 
@@ -20,7 +21,7 @@ namespace Store.G04Web
             var builder = WebApplication.CreateBuilder(args);
 
             // Add services to the container.
-
+            /*
             builder.Services.AddControllers(); // APIController
 
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -28,18 +29,21 @@ namespace Store.G04Web
             builder.Services.AddSwaggerGen();
 
             // DB
-            /*
-            builder.Services.AddDbContext<StoreDbContext>(Options =>
-            {
-                Options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
-            });
-            builder.Services.AddScoped<IDbInitializer, DbInitialize>(); // Allow DI For DbInitializer
-            builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
-            */
+
+            //builder.Services.AddDbContext<StoreDbContext>(Options =>
+            //{
+            //    Options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+            //});
+            //builder.Services.AddScoped<IDbInitializer, DbInitialize>(); // Allow DI For DbInitializer
+            //builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
+
             builder.Services.AddInfrastructureServices(builder.Configuration);
 
-            builder.Services.AddScoped<IServiceManger, ServiceManger>(); // Mapping
-            builder.Services.AddAutoMapper(M => M.AddProfile(new ProductProfile(builder.Configuration)));
+
+            //builder.Services.AddScoped<IServiceManger, ServiceManger>(); // Mapping
+            //builder.Services.AddAutoMapper(M => M.AddProfile(new ProductProfile(builder.Configuration)));
+
+            builder.Services.AddApplicationServices(builder.Configuration);
 
             builder.Services.Configure<ApiBehaviorOptions>(config =>
             {
@@ -60,10 +64,14 @@ namespace Store.G04Web
                 };
             });
 
+            */
+
+            builder.Services.RegisterAllServices(builder.Configuration);
+
             var app = builder.Build();
 
             // Configure the HTTP request pipeline.
-
+            /*
             #region Initialize Db
             using var scope = app.Services.CreateScope();
             var dbInitializer = scope.ServiceProvider.GetRequiredService<IDbInitializer>(); // ASK CLR to Create object From IDbInitializer
@@ -87,7 +95,12 @@ namespace Store.G04Web
 
             app.MapControllers();
 
+            */
+
+            await app.ConfigureMiddleWares();
+
             app.Run();
+
         }
     }
 }
