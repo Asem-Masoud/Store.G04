3607ae66cdcb12713f50f01925c4d73ccf8bfb4c
عاصم محمد محمد يوسف مسعود <125200754@sha.edu.eg>
2025-11-01 10:52:59 +0200

    Basket Module - Entities

---
+++ Core/Store.G04.Domain/Entities/Baskets/BasketItem.cs
@@ -0,0 +1,11 @@
+﻿namespace Store.G04.Domain.Entities.Baskets
+{
+    public class BasketItem
+    {
+        public int Id { get; set; }
+        public int ProductName { get; set; }
+        public string PictureUrl { get; set; }
+        public decimal Price { get; set; }
+        public int Quantity { get; set; }
+    }
+}
---
+++ Core/Store.G04.Domain/Entities/Baskets/CustomerBasket.cs
@@ -0,0 +1,9 @@
+﻿namespace Store.G04.Domain.Entities.Baskets
+{
+    public class CustomerBasket
+    {
+        public int Id { get; set; }
+        public IEnumerable<BasketItem> Items { get; set; }
+
+    }
+}
