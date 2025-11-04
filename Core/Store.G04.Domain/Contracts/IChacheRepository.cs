ce2c8f6cef0bd0651ac22d5bf9069fd412f0424a
عاصم محمد محمد يوسف مسعود <125200754@sha.edu.eg>
2025-11-04 23:49:37 +0200

    Cache Repository

---
+++ Core/Store.G04.Domain/Contracts/IChacheRepository.cs
@@ -0,0 +1,8 @@
+﻿namespace Store.G04.Domain.Contracts
+{
+    public interface IChacheRepository
+    {
+        Task<string?> GetAsync(string key);
+        Task SetAsync(string key, object value, TimeSpan duration);
+    }
+}
---
+++ Infrastructure/Persistense/Repositories/ChacheRepository.cs
@@ -0,0 +1,21 @@
+﻿using StackExchange.Redis;
+using Store.G04.Domain.Contracts;
+using System.Text.Json;
+
+namespace Store.G04.Persistence.Repositories
+{
+    public class ChacheRepository(IConnectionMultiplexer connection) : IChacheRepository
+    {
+        private readonly IDatabase _database = connection.GetDatabase();
+        public async Task<string?> GetAsync(string key)
+        {
+            var redisValue = await _database.StringGetAsync(key);
+            return redisValue;
+        }
+
+        public async Task SetAsync(string key, object value, TimeSpan duration)
+        {
+            await _database.StringSetAsync(key, JsonSerializer.Serialize(value), duration);
+        }
+    }
+}
