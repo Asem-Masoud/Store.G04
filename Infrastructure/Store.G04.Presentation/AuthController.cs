13d314693ed8c3909ba8f80c5d28664b2a773299
عاصم محمد محمد يوسف مسعود <125200754@sha.edu.eg>
2025-11-06 13:20:46 +0200

    Security Module - Auth Controller[Login-Register]

---
+++ Infrastructure/Store.G04.Presentation/AuthController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Store.G04.Services.Abstractions;
+using Store.G04.Shared.Dtos.Auth;
+using System.Threading.Tasks;
+
+namespace Store.G04.Presentation
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class AuthController(IServiceManger _serviceManger) : ControllerBase
+    {
+        // login
+        [HttpPost("login")] // POST: baseUrl/api/auth/login
+        public async Task<IActionResult> Login(LoginRequest request)
+        {
+            var result = await _serviceManger.AuthService.LoginAsync(request);
+            return Ok(result);
+        }
+
+        // register
+        [HttpPost("register")] // POST: baseUrl/api/auth/register
+        public async Task<IActionResult> Register(RegisterRequest request)
+        {
+            var result = await _serviceManger.AuthService.RegisterAsync(request);
+            return Ok(result);
+        }
+
+    }
+}
